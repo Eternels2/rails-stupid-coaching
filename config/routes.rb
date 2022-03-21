6ee145e7f70ea630ac40403d9a4874bfa5c2c843
eric zarazir <eric.zarazir@gmail.com>
2022-03-21 11:34:46 +0100

    rails new

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get 'answer', to: 'questions#answer'
+  get 'ask', to: 'questions#ask'
+  root to: 'questions#ask'
+end
